7b3fda834443cf8501c9da1bdf8da69b8236b635
bomiii1 <bomi9186@gmail.com>
2026-07-31 13:47:05 +0900

    검색 상세페이지 작업중

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,3 +1,25 @@
+import { Search as SearchIcon } from "lucide-react";
+import { useState } from "react";
+
 export default function Search() {
-  return <div className="mt-[80px]">search</div>;
+  const [keyword, setKeyword] = useState("");
+  const [data, setData] = useState([]);
+
+  const onSubmit = () => {};
+  console.log(keyword);
+  return (
+    <div className="mt-[180px] px-[150px]">
+      <div>
+        <form className="w-[22%] h-auto flex  items-center justify-between py-4 px-4 border-b-1 border-white/30">
+          <input
+            type="text"
+            placeholder="영화를 검색해보세요."
+            onChange={(e) => setKeyword(e.target.value)}
+            className="outline-none placeholder:text-white/80"
+          ></input>
+          <SearchIcon />
+        </form>
+      </div>
+    </div>
+  );
 }
